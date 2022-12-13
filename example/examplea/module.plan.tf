2233e00a546ed08dec39cd880a5d7ffe6577e920
James Woolfenden <james.woolfenden@gmail.com>
2022-12-13 16:13:57 +0000

    a better example

--- example/examplea/module.plan.tf
+++ example/examplea/module.plan.tf
@@ -2,4 +2,12 @@ module "plan" {
   source         = "../../"
   resource_group = azurerm_resource_group.resource_group
   common_tags    = var.common_tags
+  service_plan = {
+    name                     = "pike"
+    os_type                  = "Linux"
+    sku_name                 = "P1v2"
+    worker_count             = 2
+    per_site_scaling_enabled = false
+    zone_balancing_enabled   = false
+  }
 }
